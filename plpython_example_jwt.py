55dc71b3416e7876eebbefd512ee28b34f2849fa
Bryant Howell <bryantbhowell@users.noreply.github.com>
2019-12-30 15:02:45 -0600

    Added JWT example and fixed the SQL on the checks, which was ignoring everything because it needed a GROUP BY

--- plpython_example_jwt.py
+++ plpython_example_jwt.py
@@ -4,8 +4,8 @@
 # Only copy the portion below to create the actual function
 
 CREATE OR REPLACE FUNCTION
-web_service(token text, series_name text, start_date date, end_date date)
-RETURNS TABLE(like api_table_2)
+web_service_jwt(jwt_token text)
+RETURNS TABLE(like api_table_3)
 AS $$
 
 # Per Data Type Mapping documentation (https://www.postgresql.org/docs/10/plpython-data.html)
@@ -17,17 +17,18 @@ AS $$
 #
 
 # This just makes sure things aren't blank. Dates and values might have other constraints
-for arg in [token, series_name, start_date, end_date]:
+for arg in [jwt_token, ]:
     if arg == "" or arg is None:
         return []
 
 # requests library is standard for making HTTP requests
 import requests
 import datetime
+import jwt  # pyjwt library must be installed, allows parsing a JWT token
 # Name of actual table created to store the responses from the Web Service
 # Should start with a creation_timestamp and unique_request_id columns, then the
 # flattened response from the Web Service
-storage_table_name = 'api_table_2'
+storage_table_name = 'api_table_3'
 
 # REST endpoint name to be requested from
 # Also might have logic here for adding in parameters to the REST request
@@ -36,11 +37,11 @@ storage_table_name = 'api_table_2'
 # This example, start_date and end_date need to be formatted YYYY-MM-DD
 # Maybe add some text mangling to figure out the date formatting
 quandl_api_key = ''
-rest_endpoint = 'https://www.quandl.com/api/v3/datasets/CHRIS/{}/data.json'.format(series_name)
+rest_endpoint = 'https://www.quandl.com/api/v3/datasets/CHRIS/{}/data.json'
 
 # For caching and filter purposes, combine all input arguments into a single string
 # which will be included in each row
-unique_request_id = "{}_{}_{}_{}".format(token, series_name, start_date, end_date)
+unique_request_id = "{}".format(jwt_token)
 
 # Cleanup old sessions - clean everything more than 24 hours old, and then assign a
 # "session cleanup" that cleans out and rerequests from the same unique_request_id after a certain amount of time
@@ -96,12 +97,24 @@ if exists_check.nrows() == 0:
     # Make actual request to web service for data
     #
 
+    # We'll parse the JWT here
+    # Assume a JWT payload that looks like this (Yours will look like whatever it looks like)
+    # jwt_payload = {'series_name': str,
+    #              'iat': iat, # timestamp
+    #               'start_date': str,
+    #                'end_date' : str,
+    #               'username' : str
+    #               }
+    jwt_secret = 'between-you-and-me-keep-this-a-secret'
+    decoded_value = jwt.decode(jwt_token, jwt_secret, algorithm='HS256')
+    final_rest_endpoint = rest_endpoint.format(decoded_value['series_name'])
 
-    api_params = {'start_date' : start_date, 'end_date' : end_date}
+    # If you pass times as a time type, you might have to do some more time mangling to get to string
+    api_params = {'start_date' : decoded_value['start_date'], 'end_date' : decoded_value['end_date']}
     # If you have a Quandl API key, it will get placed in here
     if quandl_api_key != "":
         api_params['api_key'] = quandl_api_key
-    response = requests.get(url=rest_endpoint, params=api_params)
+    response = requests.get(url=final_rest_endpoint, params=api_params)
 
     # Some alternative possibilities depending on what your API needs (https://requests.readthedocs.io/en/master/user/quickstart/)
     # api_headers = { 'api_token' : api_token}
@@ -112,6 +125,7 @@ if exists_check.nrows() == 0:
     # Check for a valid response, if not return no rows
     # You could instead through an exception here (https://www.postgresql.org/docs/10/plpython-util.html)
     if response.status_code >= 400:
+        raise plpy.Error('REST API request no work')
         return []
     # Grab the JSON from the response (if your API is returning plain-text or XML, use response.text)
     rest_json = response.json()
